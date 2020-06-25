72a479061070407bd3790c794d34510c9401d30d
emmanuelmathot <emmanuel.mathot@gmail.com>
2020-06-25 22:52:58 +0200

    tests working with prio

--- src/DotNetStac.Test/UseCases/Sentinel2.cs
+++ src/DotNetStac.Test/UseCases/Sentinel2.cs
@@ -8,24 +8,30 @@ using Xunit;
 
 namespace Stac.Test.UseCases
 {
-    [TestCaseOrderer("DotNetStac.Test.PriorityOrderer", "Sentinel2UseCase")]
+    [TestCaseOrderer("Stac.Test.PriorityOrderer", "DotNetStac.Test")]
     public class Sentinel2 : TestBase
     {
+        private static StacCatalog catalog;
+
         [Fact, TestPriority(1)]
         public void LoadRootCatalog()
         {
-            var catalog = StacCatalog.LoadUri(GetUseCaseFileUri("catalog.json")).Result;
+            catalog = StacCatalog.LoadUri(GetUseCaseFileUri("catalog.json")).Result;
 
             Assert.NotNull(catalog);
             Assert.IsType<StacCatalog>(catalog);
             Assert.Equal("sentinel-stac", catalog.Id);
 
+        }
+
+        [Fact, TestPriority(2)]
+        public void LoadRootChildren()
+        {
             IDictionary<Uri, StacCatalog> children = catalog.GetChildren();
 
             Assert.Equal(1, children.Count);
 
             Assert.IsType<StacCollection>(children.First().Value);
-
         }
 
 
