6e8f8c315a744beccb66df9fea7fd32ad09180d8
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2020-12-09 11:27:16 +0100

    processing extension + Tests

--- src/DotNetStac.Test/Item/EoExtensionTests.cs
+++ src/DotNetStac.Test/Item/EoExtensionTests.cs
@@ -21,7 +21,7 @@ namespace Stac.Test.Item
             var k3CompleteJson = GetJson("Item", "K3A_20200508102646_28267_00027320_L1G");
             var k3MissingBandsJson = GetJson("Item", "K3A_20200508102646_28267_00027320_L1G_missing_bands");
 
-            IStacItem k3MissingBands = JsonConvert.DeserializeObject<StacItem>(k3MissingBandsJson);
+            StacItem k3MissingBands = JsonConvert.DeserializeObject<StacItem>(k3MissingBandsJson);
 
             EoStacExtension k3MissingBandsEO = k3MissingBands.GetExtension<EoStacExtension>();
 
@@ -50,6 +50,71 @@ namespace Stac.Test.Item
             JsonAssert.AreEqual(k3CompleteJson, k3MissingBandsJson);
 
 
+        }
+
+        [Fact]
+        public void CreateEoExtension()
+        {
+            var coordinates = new[]
+           {
+                new List<IPosition>
+                {
+                    new Position(37.488035566,-122.308150179),
+                    new Position(37.538869539,-122.597502109),
+                    new Position(37.613537207,-122.576687533),
+                    new Position(37.562818007,-122.288048600),
+                    new Position(37.488035566,-122.308150179)
+                }
+            };
+
+            var geometry = new Polygon(new LineString[] { new LineString(coordinates[0]) });
+
+            var properties = new Dictionary<string, object>();
+
+            properties.Add("datetime", DateTime.Parse("2016-05-03T13:21:30.040Z").ToUniversalTime());
+            properties.Add("collection", "CS3");
+
+            StacItem item = new StacItem(geometry, properties, "CS3-20160503_132130_04");
+
+            item.Links.Add(StacLink.CreateSelfLink(new Uri("http://cool-sat.com/catalog/CS3-20160503_132130_04/CS3-20160503_132130_04.json")));
+            item.Links.Add(StacLink.CreateCollectionLink(new Uri("http://cool-sat.com/catalog.json")));
+
+            item.Assets.Add("analytic", new StacAsset(new Uri("relative-path/to/analytic.tif", UriKind.Relative), null, "4-Band Analytic", null));
+            item.Assets.Add("thumbnail", StacAsset.CreateThumbnailAsset(new Uri("http://cool-sat.com/catalog/CS3-20160503_132130_04/thumbnail.png"), null, "Thumbnail"));
+
+            // item.BoundingBoxes = new double[4] { -122.59750209, 37.48803556, -122.2880486, 37.613537207 };
+            item.BoundingBoxes = item.GetBoundingBoxFromGeometryExtent();
+
+            EoStacExtension eo = new EoStacExtension(item);
+            eo.CloudCover = 0;
+
+            var actualJson = JsonConvert.SerializeObject(item);
+
+            var expectedJson = GetJson("Item");
+
+            JsonAssert.AreEqual(expectedJson, actualJson);
+        }
+
+        [Fact]
+        public void CloneEoItem()
+        {
+            var json = GetJson("Item");
+
+            var item = StacItem.LoadJToken(JsonConvert.DeserializeObject<JToken>(json), null);
+
+            item = new StacItem(item.UpgradeToCurrentVersion());
+
+            var actualJson = JsonConvert.SerializeObject(item);
+
+            JsonAssert.AreEqual(json, actualJson);
+
+            item = StacItem.LoadJToken(JsonConvert.DeserializeObject<JToken>(json), null);
+
+            item = new StacItem(item as StacItem);
+
+            actualJson = JsonConvert.SerializeObject(item);
+
+            JsonAssert.AreEqual(json, actualJson);
         }
 
     }
---
+++ src/DotNetStac.Test/Item/ProcessingExtensionTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Stac.Extensions.Sat;
+using GeoJSON.Net;
+using GeoJSON.Net.Geometry;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Stac.Item;
+using Xunit;
+using System.IO;
+using Stac.Extensions.Eo;
+using Stac.Extensions.Processing;
+
+namespace Stac.Test.Item
+{
+    public class ProcessingExtensionTests : TestBase
+    {
+        [Fact]
+        public void TestObservableDictionary()
+        {
+            var k3CompleteJson = GetJson("Item", "K3A_20200508102646_28267_00027320_L1G_software");
+            var k3MissingSoftwareJson = GetJson("Item", "K3A_20200508102646_28267_00027320_L1G_missing_software");
+
+            StacItem k3MissingSoftware = JsonConvert.DeserializeObject<StacItem>(k3MissingSoftwareJson);
+
+            ProcessingStacExtension k3MissingSoftwareProc = k3MissingSoftware.GetExtension<ProcessingStacExtension>();
+
+            Assert.Null(k3MissingSoftwareProc);
+
+            k3MissingSoftwareProc = new ProcessingStacExtension(k3MissingSoftware);
+
+            Assert.NotNull(k3MissingSoftwareProc.Software);
+
+            k3MissingSoftwareProc.Software.Add("proc_IPF", "2.0.1");
+
+            k3MissingSoftwareJson = JsonConvert.SerializeObject(k3MissingSoftware);
+
+            JsonAssert.AreEqual(k3CompleteJson, k3MissingSoftwareJson);
+
+        }
+
+    }
+}
