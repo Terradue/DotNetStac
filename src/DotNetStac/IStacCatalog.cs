ff4c15dab243ac9514a21946dd635678a51a17eb
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2021-04-10 18:18:00 +0200

    notebook update #1

--- src/DotNetStac/IStacCatalog.cs
+++ src/DotNetStac/IStacCatalog.cs
@@ -3,7 +3,7 @@ namespace Stac
     /// <summary>
     /// Common interface for all Stac catalogs (catalogs, collections)
     /// </summary>
-    public interface IStacCatalog : IStacObject
+    public interface IStacCatalog : IStacObject, IStacParent
     {
         
     }
