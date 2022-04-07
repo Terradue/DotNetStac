76a7ac2665beb0f5a7afacc5b97a398bfa8dd226
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2022-04-07 12:25:20 +0200

    alternate-assets extension + tests

---
+++ src/DotNetStac/Extensions/IStacAssetExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Stac.Collection;
+
+namespace Stac.Extensions
+{
+    public interface IStacAssetExtension
+    {
+        StacAsset StacAsset { get; }
+    }
+}
